ee765bb69d0cd09f5553b5b9d669914ab84f5b3d
Deepak Nailwal <deepaknailwal27@gmail.com>
2020-09-16 17:35:53 +0530

    first commit

---
+++ src/main/java/com/springrest/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.springrest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/sample")
+	public String getString()
+	{
+		return "Sample String";
+	}
+}
